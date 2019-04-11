1b20e762b7311fe576f321bfefd9cd93adc6379d
tomacht <thomas@flexio.fr>
2019-04-11 15:33:48 +0200

    fats first test

--- src/js/XmlHttpRequester.js
+++ src/js/XmlHttpRequester.js
@@ -1,6 +1,6 @@
 /* global fetch, Headers,Request,URL,Response */
 import {InitRequestBuilder} from './InitRequestBuilder'
-import {assertType, isString, isNull, ArrayString} from 'flexio-jshelpers'
+import {assertType, isString, isNull, StringArray} from 'flexio-jshelpers'
 import {XmlHttpResponseDelegate} from './XmlHttpResponseDelegate'
 
 /**
@@ -10,7 +10,7 @@ export class XmlHttpRequester {
   constructor() {
     /**
      *
-     * @type {Map<string, ArrayString>}
+     * @type {Map<string, string|StringArray>}
      * @private
      */
     this.__headers = new Map()
@@ -113,7 +113,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   parameter(name, value) {
-    assertType(isString(name) && (isString(value) || isNull(value)), 'FetchRequester:parameter: name and value should be string or null')
+    assertType(isString(name) && (isString(value) || isNull(value)), 'XmlHttpRequester:parameter: name and value should be string or null')
     this.__parameters.set(name, value)
     return this
   }
@@ -124,7 +124,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   arrayParameter(name, values) {
-    assertType(isString(name), 'FetchRequester:arrayParameter: name should be string or null')
+    assertType(isString(name), 'XmlHttpRequester:arrayParameter: name should be string or null')
     this.__parameters.delete(name)
     for (const v in new StringArray(...values)) {
       this.__parameters.append(name, v)
@@ -138,7 +138,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   header(name, value) {
-    assertType(isString(name) && (isString(value) || isNull(value)), 'FetchRequester:header: name and value should be string or null')
+    assertType(isString(name) && (isString(value) || isNull(value)), 'XmlHttpRequester:header: name and value should be string or null')
     this.__headers.set(name, value)
     return this
   }
@@ -149,7 +149,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   XAccount(account) {
-    assertType(isString(account) || isNull(account), 'FetchRequester:Account: `account` should be string or null')
+    assertType(isString(account) || isNull(account), 'XmlHttpRequester:Account: `account` should be string or null')
     return this.header('X-account', account)
   }
 
@@ -159,7 +159,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   AuthorizationBearer(token) {
-    assertType(isString(token) || isNull(token), 'FetchRequester:Account: `token` should be string or null')
+    assertType(isString(token) || isNull(token), 'XmlHttpRequester:Account: `token` should be string or null')
     return this.header('Authorization', 'Bearer ' + token)
   }
 
@@ -169,7 +169,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   arrayHeader(name, values) {
-    assertType(isString(name), 'FetchRequester:arrayHeader: name should be string or null')
+    assertType(isString(name), 'XmlHttpRequester:arrayHeader: name should be string or null')
     this.__headers.set(name, new StringArray(...values))
     return this
   }
@@ -179,7 +179,7 @@ export class XmlHttpRequester {
    * @return {this}
    */
   path(path) {
-    assertType(isString(path) || isNull(path), 'FetchRequester:path: path should be string or null')
+    assertType(isString(path) || isNull(path), 'XmlHttpRequester:path: path should be string or null')
     this.__path = new URL(path)
     return this
   }
@@ -192,34 +192,50 @@ export class XmlHttpRequester {
   __exec(method) {
     assertType(isString(value), 'InitRequestBuilder:method value should be a string')
     assertType(['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD'].includes(value), 'InitRequestBuilder:method value should be in [\'GET\', \'POST\', \'PATCH\', \'PUT\', \'DELETE\', \'HEAD\']')
-
-    var oReq = new XMLHttpRequest()
+    /**
+     *
+     * @type {XMLHttpRequest}
+     */
+    const oReq = new XMLHttpRequest()
     oReq.open(method, this.__buildPath())
+    // oReq.setRequestHeader(header, value)
+    // oReq.overrideMimeType(mimeType)
+
     oReq.send()
     return new XmlHttpResponseDelegate(oReq.status, oReq.responseText, new Map())
 
   }
 
+  /**
+   *
+   * @param {XMLHttpRequest} request
+   * @return {Map<string, StringArray|string>}
+   * @private
+   */
   __responseHeaders(request) {
-    var headers = request.getAllResponseHeaders()
+    const headers = request.getAllResponseHeaders()
 
-    // Convert the header string into an array
-    // of individual headers
-    var arr = headers.trim().split(/[\r\n]+/)
-
-    // Create a map of header names to values
-    var headerMap = new Map
+    const arr = headers.trim().split(/[\r\n]+/)
+    /**
+     *
+     * @type {Map<string, (StringArray|string)>}
+     */
+    const headerMap = new Map
     arr.forEach(function(line) {
-      var parts = line.split(': ')
-      var header = parts.shift()
-      var value = parts.join(': ')
+      const parts = line.split(': ')
+      const header = parts.shift()
+      const value = parts.join(': ')
       if (headerMap.has(header)) {
-
+        if (headerMap.get(header) instanceof ArrayString) {
+          headerMap.get(header).push(value)
+        } else {
+          headerMap.set(header, new ArrayString(headerMap.get(header), value))
+        }
       } else {
-
         headerMap.set(header, value)
       }
     })
+    return headerMap
   }
 
   /**
